1e650126fd9cf2c7e99eb5cff49499fed22a693a
Margarita Yagubyanc <magaitta@mail.ru>
2020-07-15 17:33:49 +0300

    update program

--- Practic10.py
+++ Practic10.py
@@ -1,4 +1,5 @@
 import numpy as np
+import datetime
 
 
 def knff(a, b, c):
@@ -9,15 +10,34 @@ def knff(a, b, c):
     return np.logical_and(part1, np.logical_and(part2, np.logical_and(part3, part4)))
 
 
+def knff2(a, b, c):
+    part1 = np.logical_or(np.logical_or(a, b), c)
+    part2 = np.logical_or(np.logical_or(np.logical_not(a), np.logical_not(b)), np.logical_not(c))
+    return np.logical_and(part1, part2)
+
+def knff3(a, b, c):
+    part1 = np.logical_or(np.logical_or(a, b), c)
+    part2 = np.logical_or(np.logical_or(np.logical_not(a), np.logical_not(b)), np.logical_not(c))
+    part3 = np.logical_or(np.logical_or(a, np.logical_not(b)), c)
+    part4 = np.logical_or(np.logical_or(a, b), np.logical_not(c))
+    part5 = np.logical_or(np.logical_or(np.logical_not(a), b), c)
+    part6 = np.logical_or(np.logical_or(a, np.logical_not(b)), np.logical_not(c))
+    part7 = np.logical_or(np.logical_or(np.logical_not(a), b), np.logical_not(c))
+    part8 = np.logical_or(np.logical_or(np.logical_not(a), np.logical_not(b)), c)
+    return np.logical_and(part1, np.logical_and(part2, np.logical_and(part3, np.logical_and(part4,np.logical_and(part5, np.logical_and(part6, np.logical_and(part7, part8)))))))
+
 if __name__ == '__main__':
-    print('Hello, world!')
     a = np.array([False, False, False, False, True, True, True, True])
     b = np.array([False, False, True, True, False, False, True, True])
     c = np.array([False, True, False, True, False, True, False, True])
-    knf = knff(a, b, c)
+    t1=datetime.datetime.now()
+    knf = knff3(a, b, c)
+    print(datetime.datetime.now()-t1)
     print(knf)
+
     indexes = np.where(knf==True)
-    if len(indexes)==0:
+    print(indexes)
+    if len(indexes[0])==0:
         print('not exist')
     else:
         print('exist')
